a1276c5742d4da945355a64fe7be27ea1cc22fb4
Vladimir Dyadenko <Stilmagroup@gmail.com>
2025-03-05 14:03:09 +0200

    add mongoDB connection string

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+config();
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 3000);
+  const PORT = `${process.env.PORT}` || 5000;
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.enableCors({
+    origin: [ 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
+  await app.listen(PORT, () => console.log(`Server started on port=${PORT}`));
 }
 bootstrap();
